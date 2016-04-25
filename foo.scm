ab1c57c76b5d127d414e5a145dfc52b49b1970dd
My Name <nobody@itsthe.crabdance.com>
2016-04-25 01:40:08 -0500

    adding 'let'

--- foo.scm
+++ foo.scm
@@ -30,6 +30,13 @@
        (not (null? x))
        (member? (car x) '(add1 + -))))
 
+(define (let? x)
+  (and (list? x)
+       (not (null? x))
+       (eq? (car x) 'let)))
+
+(define variable? symbol?)
+
 (define (primcall-op x)
   (car x))
 
@@ -47,34 +54,77 @@
 
         (true "error: no immediate representation eh")))
 
-(define (emit-primitive-call x sindex)
+(define (emit-primitive-call x sindex env)
   (let ((op (primcall-op x)))
     (emit-comment "primcall " op ": " x)
     (cond
       ((eq? op 'add1)
-       (emit-expr (primcall-op-1 x) sindex)
+       (emit-expr (primcall-op-1 x) sindex env)
        (emit "add rax, " (immediate-rep 1)))
 
       ((eq? op '+)
-       (emit-expr (primcall-op-2 x) sindex)
+       (emit-expr (primcall-op-2 x) sindex env)
        (emit "mov [rsp - " sindex "], rax")
-       (emit-expr (primcall-op-1 x) (+ sindex 8))
+       (emit-expr (primcall-op-1 x) (+ sindex 8) env)
        (emit "add rax, [rsp - " sindex "]"))
 
       ((eq? op '-)
-       (emit-expr (primcall-op-2 x) sindex)
+       (emit-expr (primcall-op-2 x) sindex env)
        (emit "mov [rsp - " sindex "], rax")
-       (emit-expr (primcall-op-1 x) (+ sindex 8))
+       (emit-expr (primcall-op-1 x) (+ sindex 8) env)
        (emit "sub rax, [rsp - " sindex "]")))))
 
-(define (emit-expr x sindex)
+(define (extend-env var sindex env)
+  (cons (list var sindex) env))
+
+(define (lookup x env)
+  (emit-comment "trying " x " at " env)
+  (cond ((null? env)
+         'not-found-sorry)
+
+        ((eq? (caar env) x)
+         (cadar env))
+
+        (true
+          (lookup x (cdr env)))))
+
+(define (emit-let bindings body sindex env)
+  (define (f b* new-env sindex)
+    (emit-comment b* " " sindex " " new-env)
+    (cond
+      ((null? b*)
+       (emit-comment "got here, " body)
+       (emit-expr body sindex new-env))
+
+      (true
+        (let ((b (car b*)))
+          (emit-expr (cadr b) sindex env)
+          (emit "mov [rsp - " sindex "], eax")
+          (f (cdr b*)
+             (extend-env (car b) sindex new-env)
+             (+ sindex 8))))))
+  
+  (f bindings env sindex))
+
+(define bindings cadr)
+(define body     caddr)
+
+(define (emit-expr x sindex env)
   (cond
     ((immediate? x)
      (emit-comment "immediate int " x)
      (emit "mov rax, " (immediate-rep x)))
 
+    ((let? x)
+     (emit-comment "got here")
+     (emit-let (bindings x) (body x) sindex env))
+
+    ((variable? x)
+     (emit-comment "variable " x)
+     (emit "mov rax, [rsp - " (lookup x env) "]"))
+
     ((primcall? x)
-     (emit-primitive-call x sindex))
+     (emit-primitive-call x sindex env))
 
     (true (print "wut"))))
 
@@ -82,8 +132,11 @@
   (emit-flag "bits 64")
   (emit-flag "global scheme_thing")
   (emit-flag "scheme_thing:")
-  (emit-expr x 8) ; 8 so we don't overwrite the return pointer
+  (emit-expr x 8 '()) ; 8 so we don't overwrite the return pointer
   (emit "ret"))
 
+;(compile-program '(+ 2 (- (+ 40 40) 40)))
 (compile-program
-  '(+ 2 (- (+ 40 40) 40)))
+  '(let ((x 20)
+         (y 60))
+     (- (+ x y) 38)))
